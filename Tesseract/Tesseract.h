a765acc55ced1629180b1acca8d879a1c7c633f0
Rob Rix <rob.rix@github.com>
2014-12-15 17:40:28 -0500

    Initial Commit

---
+++ Tesseract/Tesseract.h
@@ -0,0 +1,8 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+/// Project version number for Tesseract.
+extern double TesseractVersionNumber;
+
+/// Project version string for Tesseract.
+extern const unsigned char TesseractVersionString[];
+
