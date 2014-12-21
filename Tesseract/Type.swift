6c9ce1b15db33837c9f5a18502f9e377174e3981
Rob Rix <rob.rix@github.com>
2014-12-20 21:12:32 -0500

    Type is Equatable.

--- Tesseract/Type.swift
+++ Tesseract/Type.swift
@@ -1,6 +1,6 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
-public enum Type {
+public enum Type: Equatable {
 	case Boolean
 	case Integer
 	case String
@@ -12,6 +12,31 @@ public enum Type {
 
 	case Sum([Type])
 	case Product([Type])
+}
+
+
+public func == (left: Type, right: Type) -> Bool {
+	switch (left, right) {
+	case (.Boolean, .Boolean), (.Integer, .Integer), (.String, .String):
+		return true
+
+	case let (.Function(x1, y1), .Function(x2, y2)):
+		return x1 == x2 && y1 == y2
+
+	case let (.Generic(n1, x1), .Generic(n2, x2)):
+		return n1 == n2 && x1 == x2
+	// fixme: this is insufficient, we need to know the context
+	case let (.Parameter(x), .Parameter(y)):
+		return x == y
+
+	case let (.Sum(xs), .Sum(ys)):
+		return xs == ys
+	case let (.Product(xs), .Product(ys)):
+		return xs == ys
+
+	default:
+		return false
+	}
 }
 
 
