079317cfda450b95dad26a959c28962a0c3e8906
Rob Rix <rob.rix@github.com>
2014-12-25 20:25:51 -0500

    Construct and test the type of the identity term.

--- TesseractTests/TypeTests.swift
+++ TesseractTests/TypeTests.swift
@@ -1,5 +1,6 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
+import Box
 import Prelude
 import Tesseract
 import XCTest
@@ -7,5 +8,9 @@ import XCTest
 final class TypeTests: XCTestCase {
 	func testConstantTermHasConstantType() {
 		XCTAssertEqual(typeof(.Constant(.Integer(0))).either(const(.Boolean), id), Type.Integer)
+	}
+
+	func testIdentityTermHasPolymorphicType() {
+		assertEqual(typeof(.Abstraction(.Parameter(0), Box(.Variable(0)))), Type.Polymorphic(1, Box(.Function(Box(.Parameter(0)), Box(.Parameter(0))))))
 	}
 }
