78c844f51e785ee882798248b83e8d919d279323
Rob Rix <rob.rix@github.com>
2014-12-29 17:54:29 -0500

    Test node identifiers.

---
+++ TesseractTests/IdentifierTests.swift
@@ -0,0 +1,11 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+import Tesseract
+import XCTest
+
+final class IdentifierTests: XCTestCase {
+	func testNodeStringValueIsUUIDString() {
+		let identifier = Identifier()
+		XCTAssertEqual(countElements(identifier.stringValue), 36)
+	}
+}
