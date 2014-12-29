fc92e877b514a7b9784b51f731ba5d0229ad0795
Rob Rix <rob.rix@github.com>
2014-12-29 17:34:59 -0500

    Move Identifier to its own file.

--- Tesseract/Graph.swift
+++ Tesseract/Graph.swift
@@ -1,39 +1,5 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
-public enum Identifier: Hashable {
-	// MARK: Cases
-
-	case Parameter(Int)
-	case Return(Int)
-	case Node(String)
-
-
-	// MARK: API
-
-	public var stringValue: String {
-		switch self {
-		case let Parameter(x):
-			return toString(x)
-		case let Return(x):
-			return toString(x)
-		case let Node(x):
-			return x
-		}
-	}
-
-
-	// MARK: Hashable
-
-	public var hashValue: Int {
-		return self.stringValue.hashValue
-	}
-}
-
-public func == (left: Identifier, right: Identifier) -> Bool {
-	return left.stringValue == right.stringValue
-}
-
-
 public struct Edge {
 	public init(input: Identifier, output: Identifier) {
 		self.input = input
