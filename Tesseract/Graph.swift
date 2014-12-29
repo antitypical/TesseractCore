1a08b6ac3509cd1fd8b65ed5e59e9891b8e966f8
Rob Rix <rob.rix@github.com>
2014-12-29 17:21:06 -0500

    Identifier is an enum.

--- Tesseract/Graph.swift
+++ Tesseract/Graph.swift
@@ -1,20 +1,36 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
-public struct Identifier: Hashable {
-	// MARK: Hashable
+public enum Identifier: Hashable {
+	// MARK: Cases
 
-	public var hashValue: Int {
-		return value.hashValue
+	case Parameter(Int)
+	case Return(Int)
+	case Node(String)
+
+
+	// MARK: API
+
+	public var stringValue: String {
+		switch self {
+		case let Parameter(x):
+			return toString(x)
+		case let Return(x):
+			return toString(x)
+		case Node:
+			return ""
+		}
 	}
 
 
-	// MARK: Private
+	// MARK: Hashable
 
-	private let value: String
+	public var hashValue: Int {
+		return self.stringValue.hashValue
+	}
 }
 
 public func == (left: Identifier, right: Identifier) -> Bool {
-	return left.value == right.value
+	return left.stringValue == right.stringValue
 }
 
 
