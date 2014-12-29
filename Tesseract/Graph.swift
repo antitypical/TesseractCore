9efc9cef527a0593369c721707460cc95875c3cd
Rob Rix <rob.rix@github.com>
2014-12-29 11:20:19 -0500

    Identifiers are their own type.

--- Tesseract/Graph.swift
+++ Tesseract/Graph.swift
@@ -1,6 +1,16 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
-typealias Identifier = String
+public struct Identifier: Hashable {
+	private let value: String
+
+	public var hashValue: Int {
+		return value.hashValue
+	}
+}
+
+public func == (left: Identifier, right: Identifier) -> Bool {
+	return left.value == right.value
+}
 
 public struct Node {
 	let title: String
