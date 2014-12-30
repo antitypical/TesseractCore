014832c9c67d1a092575522ef070a0934a8021e7
Rob Rix <rob.rix@github.com>
2014-12-30 13:26:47 -0500

    Edge is Hashable.

--- Tesseract/Graph.swift
+++ Tesseract/Graph.swift
@@ -1,6 +1,6 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
-public struct Edge {
+public struct Edge: Hashable {
 	public init(input: SourceIdentifier, output: DestinationIdentifier) {
 		self.input = input
 		self.output = output
@@ -8,7 +8,19 @@ public struct Edge {
 
 	public let input: SourceIdentifier
 	public let output: DestinationIdentifier
+
+
+	// MARK: Hashable
+
+	public var hashValue: Int {
+		return input.hashValue ^ output.hashValue
+	}
 }
+
+public func == (left: Edge, right: Edge) -> Bool {
+	return left.input == right.input && left.output == right.output
+}
+
 
 public struct Graph {
 	public init(title: String, nodes: [Identifier] = [], edges: [Edge] = []) {
