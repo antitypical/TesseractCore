7334fd5d5fb40a7c6b2b4422e4964ed3a263afd4
Rob Rix <rob.rix@github.com>
2015-01-04 09:07:29 -0500

    Test application typechecking.

--- TesseractTests/TypeTests.swift
+++ TesseractTests/TypeTests.swift
@@ -1,6 +1,6 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
-import Box
+import Either
 import Prelude
 import Tesseract
 import XCTest
@@ -12,5 +12,12 @@ final class TypeTests: XCTestCase {
 
 	func testIdentityTermHasPolymorphicType() {
 		assertEqual(typeof(.abstraction(.Parameter(0), .Variable(0))), Type.function(.Parameter(0), .Parameter(0)))
+	}
+
+	func testApplicationTermTypechecks() {
+		let term = Term.abstraction(.function(.Parameter(0), .Parameter(1)), .abstraction(.Parameter(0), .application(.Variable(0), .Variable(1))))
+		// (a -> b) -> a -> b
+		let type = typeof(term)
+		assertEqual(typeof(term), Type.function(.function(.Parameter(0), .Parameter(1)), .function(.Parameter(0), .Parameter(1))))
 	}
 }
