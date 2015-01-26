91339a0df94662a1f807bc820e59b1ec0878c0fe
Rob Rix <rob.rix@github.com>
2015-01-25 21:57:15 -0500

    Symbols are Hashable.

--- Tesseract/Symbol.swift
+++ Tesseract/Symbol.swift
@@ -1,7 +1,24 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
-public struct Symbol {
+public struct Symbol: Hashable {
 	public let name: String
 	public let parameters: [String]
 	public let returns: [String]
+
+
+	// MARK: Hashable
+
+	public var hashValue: Int {
+		return
+			name.hashValue
+		^	parameters.reduce(0) { $0 ^ $1.hashValue }
+		^	returns.reduce(0) { $0 ^ $1.hashValue }
+	}
+}
+
+public func == (left: Symbol, right: Symbol) -> Bool {
+	return
+		left.name == right.name
+	&&	left.parameters == right.parameters
+	&&	left.returns == right.returns
 }
