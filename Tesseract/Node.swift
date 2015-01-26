72840d977f19216f9bffb5c29d06abd9f3c39dbd
Rob Rix <rob.rix@github.com>
2015-01-25 22:08:52 -0500

    Nodes have inputs and outputs.

--- Tesseract/Node.swift
+++ Tesseract/Node.swift
@@ -4,4 +4,26 @@ public enum Node {
 	case Parameter(String)
 	case Return(String)
 	case Abstraction(Symbol)
+
+	public var inputs: [String] {
+		switch self {
+		case Parameter:
+			return []
+		case let Return(name):
+			return [ name ]
+		case let Abstraction(symbol):
+			return symbol.parameters
+		}
+	}
+
+	public var outputs: [String] {
+		switch self {
+		case let Parameter(name):
+			return [ name ]
+		case Return:
+			return []
+		case let Abstraction(symbol):
+			return symbol.returns
+		}
+	}
 }
