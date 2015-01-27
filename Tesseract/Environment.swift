5ee2c90176f84ed9e5035313c92e70585ce7b2e3
Rob Rix <rob.rix@github.com>
2015-01-26 20:31:04 -0500

    Bind the identity function in the default environment.

--- Tesseract/Environment.swift
+++ Tesseract/Environment.swift
@@ -2,9 +2,16 @@
 
 public typealias Environment = [Symbol: Binding]
 
-public let Prelude: Environment = [:]
+public let Prelude: Environment = [
+	Symbol(name: "identity", parameters: 1, returns: 1): .Function(.Parameter(0), .Parameter(0), id),
+]
 
 public enum Binding {
 	case Constant(Type, Any)
 	case Function(Type, Type, Any -> Any)
 }
+
+
+// MARK: - Imports
+
+import Prelude
