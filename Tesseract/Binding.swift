4c5665105701d200f2643e3daac950ab3bfe1fad
Rob Rix <rob.rix@github.com>
2015-01-26 20:46:40 -0500

    Move Binding into its own file.

---
+++ Tesseract/Binding.swift
@@ -0,0 +1,15 @@
+//  Copyright (c) 2015 Rob Rix. All rights reserved.
+
+public enum Binding {
+	case Constant(Type, Any)
+	case Function(Type, Type, Any -> Any)
+
+	public var type: Type {
+		switch self {
+		case let Constant(type, _):
+			return type
+		case let Function(a, b, _):
+			return Type(function: a, b)
+		}
+	}
+}
--- Tesseract/Environment.swift
+++ Tesseract/Environment.swift
@@ -6,20 +6,6 @@ public let Prelude: Environment = [
 	Symbol(name: "identity", parameters: 1, returns: 1): .Function(.Parameter(0), .Parameter(0), id),
 ]
 
-public enum Binding {
-	case Constant(Type, Any)
-	case Function(Type, Type, Any -> Any)
-
-	public var type: Type {
-		switch self {
-		case let Constant(type, _):
-			return type
-		case let Function(a, b, _):
-			return Type(function: a, b)
-		}
-	}
-}
-
 
 // MARK: - Imports
 
