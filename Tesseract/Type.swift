4aa03174c1ad9bd6568cba2eeda0b43cc11b6dd1
Rob Rix <rob.rix@github.com>
2015-01-26 20:36:30 -0500

    Unboxed constructor for function types.

--- Tesseract/Type.swift
+++ Tesseract/Type.swift
@@ -1,6 +1,10 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
 public enum Type {
+	public init(function from: Type, _ to: Type) {
+		self = Function(Box(from), Box(to))
+	}
+
 	case Parameter(Int)
 	case Function(Box<Type>, Box<Type>)
 }
