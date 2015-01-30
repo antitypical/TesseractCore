bbb1840ce0685fcc84a57d93bafd24cdbf4827bd
Rob Rix <rob.rix@github.com>
2015-01-30 01:23:11 -0500

    Symbols are an enum.
    This enables the use of indexed symbols for parameters.

--- Tesseract/Symbol.swift
+++ Tesseract/Symbol.swift
@@ -1,13 +1,36 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
-public struct Symbol: Hashable {
+public enum Symbol: Hashable {
 	public init(_ name: String, _ type: Type) {
-		self.name = name
-		self.type = type
+		self = Named(name, type)
 	}
 
-	public let name: String
-	public let type: Type
+	public init(_ index: Int, _ type: Type) {
+		self = Parameter(index, type)
+	}
+
+
+	case Named(String, Type)
+	case Parameter(Int, Type)
+
+
+	public var name: String {
+		switch self {
+		case let Named(name, _):
+			return name
+		case let Parameter(index, _):
+			return index.description
+		}
+	}
+
+	public var type: Type {
+		switch self {
+		case let Named(_, type):
+			return type
+		case let Parameter(_, type):
+			return type
+		}
+	}
 
 
 	// MARK: Hashable
