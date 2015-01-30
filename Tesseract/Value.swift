ff5a0edfee74c6350fe208324b5345f4ed783ffc
Rob Rix <rob.rix@github.com>
2015-01-30 02:22:02 -0500

    Destructure is public.

--- Tesseract/Value.swift
+++ Tesseract/Value.swift
@@ -4,7 +4,7 @@ public enum Value {
 	case Constant(Any)
 	case Function(Any -> Any)
 
-	func destructure<T, U, V, W>(constant: T -> W, _ function: (U -> V) -> W) -> W {
+	public func destructure<T, U, V, W>(constant: T -> W, _ function: (U -> V) -> W) -> W {
 		switch self {
 		case let Constant(v):
 			return constant(v as T)
