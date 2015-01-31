f4281cf32ee7032317831efc93881956c9745e26
Rob Rix <rob.rix@github.com>
2015-01-31 17:46:10 -0500

    Define Boolean types as equal.

--- Tesseract/Type.swift
+++ Tesseract/Type.swift
@@ -55,7 +55,7 @@ public func == (left: Type, right: Type) -> Bool {
 		return x == y
 	case let (.Function(x1, x2), .Function(y1, y2)):
 		return x1 == y1 && x2 == y2
-	case (.Unit, .Unit):
+	case (.Unit, .Unit), (.Boolean, .Boolean):
 		return true
 
 	default:
