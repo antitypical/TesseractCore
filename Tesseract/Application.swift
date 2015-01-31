3225eb4ea961f4f1701574afb529c399cb518ba6
Rob Rix <rob.rix@github.com>
2015-01-31 18:48:06 -0500

    apply() returns Either.

--- Tesseract/Application.swift
+++ Tesseract/Application.swift
@@ -1,5 +1,10 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
-public func apply(value: Value, parameters: [(Edge.Destination, Node)]) -> Value {
-	return value
+public func apply(value: Value, parameters: [(Edge.Destination, Node)]) -> Either<Error, Value> {
+	return .right(value)
 }
+
+
+// MARK: - Imports
+
+import Either
