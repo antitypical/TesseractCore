fe881cef45b66fec400dd9232f05deb175cdd875
Rob Rix <rob.rix@github.com>
2015-01-31 10:08:21 -0500

    Test that a unit node evaluates to its constant.

---
+++ TesseractTests/EnvironmentTests.swift
@@ -0,0 +1,15 @@
+//  Copyright (c) 2015 Rob Rix. All rights reserved.
+
+import Prelude
+import Tesseract
+import XCTest
+
+final class EnvironmentTests: XCTestCase {
+	func testNodeReferencingConstantEvaluatesToConstant() {
+		let a = Identifier()
+		let graph = Graph(nodes: [ a: Node.Abstraction(Prelude["unit"]!.0) ])
+		let evaluated = evaluate(graph, a)
+
+		assertNotNil(assertRight(evaluated)?.constant(id as () -> ()))
+	}
+}
