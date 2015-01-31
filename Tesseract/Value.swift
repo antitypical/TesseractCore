f8fb7d7e71d686fff8ce7cdfe3db5febe7392ceb
Rob Rix <rob.rix@github.com>
2015-01-31 10:06:28 -0500

    Guard constant casts with `is`.
    Fixes a dynamicCast exception when destructuring a constant value.

--- Tesseract/Value.swift
+++ Tesseract/Value.swift
@@ -14,7 +14,7 @@ public enum Value: Printable {
 
 	public func constant<T, U>(f: T -> U) -> U? {
 		switch self {
-		case let Constant(v):
+		case let Constant(v) where v.value is T:
 			return f(v.value as T)
 		default:
 			return nil
