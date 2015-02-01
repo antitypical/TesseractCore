2e449dc860dc8e32c3154a144810abf4713f7ae6
Rob Rix <rob.rix@github.com>
2015-02-01 10:19:09 -0500

    Error check the number of parameters.

--- Tesseract/Application.swift
+++ Tesseract/Application.swift
@@ -5,6 +5,7 @@ public func apply(value: Value, identifier: Identifier, symbol: Symbol, paramete
 		reduce(parameters, (-1, nil as Either<Error<Identifier>, Memo<Value>>?)) { into, each in
 			(each.0, into.1 ?? (each.0 != into.0 ? nil : error("expected 0 or 1 edges to input \(each.0), but multiple were found \(each.0)", identifier)))
 		}.1
+	??	(parameters.last.map { $0.0 < symbol.type.arity ? nil : error("too many input edges (\($0.0 - 1))", identifier) } ?? nil)
 	??	.right(Memo(value))
 }
 
