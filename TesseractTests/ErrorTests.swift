5aa1516632c9db8ff57297fa2836f02b667aa194
Rob Rix <rob.rix@github.com>
2015-02-01 00:24:54 -0500

    Test branch error printing.

--- TesseractTests/ErrorTests.swift
+++ TesseractTests/ErrorTests.swift
@@ -8,4 +8,9 @@ final class ErrorTests: XCTestCase {
 		let error = Error("a", 0)
 		XCTAssertEqual(error.description, "0: a")
 	}
+
+	func testBranchErrorsPrintAcrossLines() {
+		let error = Error(Error("a", 0), Error(Error("b", 1), Error("c", 2)))
+		XCTAssertEqual(error.description, "0: a\n1: b\n2: c")
+	}
 }
