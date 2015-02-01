f7a75fac7181112a300ed13597f420cedc5e5afd
Rob Rix <rob.rix@github.com>
2015-01-31 23:49:36 -0500

    Error constructors.

--- Tesseract/Error.swift
+++ Tesseract/Error.swift
@@ -1,12 +1,21 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
 public enum Error {
+	public init(_ reason: String, _ from: Identifier) {
+		self = Leaf(from, reason)
+	}
+
+	public init(_ error1: Error, _ error2: Error) {
+		self = Branch(Box(error1), Box(error2))
+	}
+
+
 	case Leaf(Identifier, String)
 	case Branch(Box<Error>, Box<Error>)
 }
 
 internal func error(reason: String, from: Identifier) -> Either<Error, Memo<Value>> {
-	return .left(.Leaf(from, reason))
+	return .left(Error(reason, from))
 }
 
 
