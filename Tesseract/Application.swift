a9c7e9b5f7d45a98e40d9f49091c4591687f51ca
Rob Rix <rob.rix@github.com>
2015-02-01 10:00:34 -0500

    Check for >1 edges connected to same input.

--- Tesseract/Application.swift
+++ Tesseract/Application.swift
@@ -1,7 +1,11 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
 public func apply(value: Value, identifier: Identifier, symbol: Symbol, parameters: [(Int, Memo<Value>)]) -> Either<Error<Identifier>, Memo<Value>> {
-	return .right(Memo(value))
+	return
+		reduce(parameters, (-1, nil as Either<Error<Identifier>, Memo<Value>>?)) { into, each in
+			(each.0, into.1 ?? (each.0 != into.0 ? nil : error("expected 0 or 1 edges to input \(each.0), but multiple were found \(each.0)", identifier)))
+		}.1
+	??	.right(Memo(value))
 }
 
 
