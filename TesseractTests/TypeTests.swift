403332186571d8b340f92095731540d59ca2d5c4
Rob Rix <rob.rix@github.com>
2015-02-01 00:28:17 -0500

    Test binary function arity.

--- TesseractTests/TypeTests.swift
+++ TesseractTests/TypeTests.swift
@@ -11,4 +11,8 @@ final class TypeTests: XCTestCase {
 	func testUnaryFunctionTypesHaveArityOne() {
 		XCTAssertEqual((.Unit --> .Unit).arity, 1)
 	}
+
+	func testBinaryFunctionTypesHaveArityTwo() {
+		XCTAssertEqual((.Unit --> .Unit --> .Unit).arity, 2)
+	}
 }
