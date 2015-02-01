5e8f88be9096677c96c31f32150646f834c91682
Rob Rix <rob.rix@github.com>
2015-02-01 00:27:43 -0500

    Test unary function arity.

--- TesseractTests/TypeTests.swift
+++ TesseractTests/TypeTests.swift
@@ -7,4 +7,8 @@ final class TypeTests: XCTestCase {
 	func testConstantTypesHaveArityZero() {
 		XCTAssertEqual(Type.Unit.arity, 0)
 	}
+
+	func testUnaryFunctionTypesHaveArityOne() {
+		XCTAssertEqual((.Unit --> .Unit).arity, 1)
+	}
 }
