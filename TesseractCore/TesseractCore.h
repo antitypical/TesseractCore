7b7af8d42001a957057c1745a77f049b8a8ae4b5
Rob Rix <rob.rix@github.com>
2015-02-01 13:27:59 -0500

    Rename the header.

---
+++ TesseractCore/TesseractCore.h
@@ -0,0 +1,7 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+/// Project version number for TesseractCore.
+extern double TesseractCoreVersionNumber;
+
+/// Project version string for TesseractCore.
+extern const unsigned char TesseractCoreVersionString[];
