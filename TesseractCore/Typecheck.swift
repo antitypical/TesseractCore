85423dda542d3990bb25b0f37c2df0eca59fcee6
Rob Rix <rob.rix@github.com>
2015-02-03 22:43:08 -0500

    Typecheck with respect to an environment.

--- TesseractCore/Typecheck.swift
+++ TesseractCore/Typecheck.swift
@@ -1,13 +1,13 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
-public func typecheck(graph: Graph<Node>, from: Identifier, _ context: [Identifier: Type] = [:]) -> Either<Error<Identifier>, Type> {
+public func typecheck(graph: Graph<Node>, from: Identifier, _ environment: Environment = Prelude, _ context: [Identifier: Type] = [:]) -> Either<Error<Identifier>, Type> {
 	return
 		context[from].map(Either.right)
-	??	graph.nodes[from].map { typecheck(graph, from, $0, context) }
+	??	graph.nodes[from].map { typecheck(graph, from, $0, environment, context) }
 	??	error("could not find identifier in graph", from)
 }
 
-private func typecheck(graph: Graph<Node>, from: Identifier, node: Node, context: [Identifier: Type]) -> Either<Error<Identifier>, Type> {
+private func typecheck(graph: Graph<Node>, from: Identifier, node: Node, environment: Environment, context: [Identifier: Type]) -> Either<Error<Identifier>, Type> {
 	switch node {
 	case let .Parameter(symbol):
 		return .right(symbol.type)
