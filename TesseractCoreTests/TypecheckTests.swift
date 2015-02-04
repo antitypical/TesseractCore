36da8fa0b75ea262ae4bc9e2522afc654cead789
Rob Rix <rob.rix@github.com>
2015-02-04 17:16:53 -0500

    Test whether id(true) typechecks.
    It doesn‘t, because it’s a → a, not Boolean → Boolean, and we don’t yet
    perform unification.

--- TesseractCoreTests/TypecheckTests.swift
+++ TesseractCoreTests/TypecheckTests.swift
@@ -1,6 +1,7 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
 import Either
+import Prelude
 import TesseractCore
 import XCTest
 
@@ -10,5 +11,13 @@ final class TypecheckTests: XCTestCase {
 		let symbol = Symbol.Named("true", .Boolean)
 		let graph = Graph(nodes: [ a: Node.Symbolic(symbol) ])
 		assertEqual(assertRight(typecheck(graph, a, [symbol: Value(constant: true)])), Type.Boolean)
+	}
+
+	func testBoundFunctionNodesTypecheck() {
+		let (a, b) = (Identifier(), Identifier())
+		let identitySymbol = Symbol.Named("identity", 0 --> 0)
+		let inputSymbol = Symbol.Named("true", .Boolean)
+		let graph = Graph(nodes: [ a: Node.Symbolic(inputSymbol), b: Node.Symbolic(identitySymbol) ], edges: [ Edge((a, 0), (b, 0)) ])
+		assertEqual(assertRight(typecheck(graph, b, [ inputSymbol: Value(constant: true), identitySymbol: Value(function: id as Any -> Any) ])), Type.Boolean)
 	}
 }
