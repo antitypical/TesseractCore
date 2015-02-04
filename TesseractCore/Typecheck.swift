39f856eb9e0ba872b5fa2d0dd0e1c97b531b24ad
Rob Rix <rob.rix@github.com>
2015-02-03 22:39:21 -0500

    Stub in typechecking.

---
+++ TesseractCore/Typecheck.swift
@@ -0,0 +1,17 @@
+//  Copyright (c) 2015 Rob Rix. All rights reserved.
+
+public func typecheck(graph: Graph<Node>, from: Identifier, _ context: [Identifier: Type] = [:]) -> Either<Error<Identifier>, Type> {
+	return
+		context[from].map(Either.right)
+	??	graph.nodes[from].map { typecheck(graph, from, $0, context) }
+	??	error("could not find identifier in graph", from)
+}
+
+private func typecheck(graph: Graph<Node>, from: Identifier, node: Node, context: [Identifier: Type]) -> Either<Error<Identifier>, Type> {
+	return error("unimplmented", from)
+}
+
+
+// MARK: - Imports
+
+import Either
