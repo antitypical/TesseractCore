01e1bef0458ec94fc2be01773cb5f625d557ce55
Rob Rix <rob.rix@github.com>
2015-02-05 00:18:47 -0500

    Apparently test cases _will_ compile with the imports at the bottom.

--- TesseractCoreTests/ExportingTests.swift
+++ TesseractCoreTests/ExportingTests.swift
@@ -1,8 +1,5 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
-import TesseractCore
-import XCTest
-
 final class ExportingTests: XCTestCase {
     func testExporting() {
         let (a, b) = (Identifier(), Identifier())
@@ -10,3 +7,9 @@ final class ExportingTests: XCTestCase {
         XCTAssertEqual(export(graph), "digraph tesseract {\n\t\(a) -> \(b);\n}")
     }
 }
+
+
+// MARK: - Imports
+
+import TesseractCore
+import XCTest
