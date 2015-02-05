d268bc189ec0815c1e5a60747ebda99b55e83d72
Rob Rix <rob.rix@github.com>
2015-02-05 00:12:51 -0500

    Inline the fold.

--- TesseractCore/Exporting.swift
+++ TesseractCore/Exporting.swift
@@ -1,12 +1,8 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
-private func concat(strings: [String]) -> String {
-    return reduce(strings, "", +)
-}
-
 public func export<T>(graph: Graph<T>) -> String {
     var result = "digraph tesseract {\n"
-    result += concat(map(graph.edges, { edge in "\t" + edge.source.identifier.description + " -> " + edge.destination.identifier.description + ";\n" }))
+    result += reduce(map(graph.edges, { edge in "\t" + edge.source.identifier.description + " -> " + edge.destination.identifier.description + ";\n" }), "", +)
     result += "}"
     return result
 }
