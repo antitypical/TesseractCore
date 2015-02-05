ed9936b25d6c598ad91d6578403784cd954862e8
Rob Rix <rob.rix@github.com>
2015-02-05 00:15:06 -0500

    Append immutably.

--- TesseractCore/Exporting.swift
+++ TesseractCore/Exporting.swift
@@ -1,12 +1,12 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
 public func export<T>(graph: Graph<T>) -> String {
-    var result = "digraph tesseract {\n"
-	result += reduce(graph.edges, "") {
-		$0 + "\t" + $1.source.identifier.description + " -> " + $1.destination.identifier.description + ";\n"
-	}
-    result += "}"
-    return result
+	return
+		"digraph tesseract {\n"
+	+	reduce(graph.edges, "") {
+			$0 + "\t" + $1.source.identifier.description + " -> " + $1.destination.identifier.description + ";\n"
+		}
+	+	"}"
 }
 
 
