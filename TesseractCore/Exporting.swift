dffc2f2a7d96ac469a20555071736ce5f4057b26
Alexander Mackworth <alexander@tssr.co>
2015-02-11 22:09:47 -0800

    Export node contents, not just identifiers.

--- TesseractCore/Exporting.swift
+++ TesseractCore/Exporting.swift
@@ -1,8 +1,16 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
-public func exportDOT<T>(graph: Graph<T>) -> String {
-	let edges = join("\n", lazy(graph.edges).map { "\t\"" + $0.source.identifier.description + "\" -> \"" + $0.destination.identifier.description + "\";" })
-	return "digraph tesseract {\n\(edges)\n}"
+public func exportDOT<T: Printable>(graph: Graph<T>) -> String {
+    let edges = join("\n", lazy(graph.edges).map { edge in
+        let sourceID = edge.source.identifier
+        let destinationID = edge.destination.identifier
+        let source = graph.nodes[sourceID]
+        let destination = graph.nodes[destinationID]
+        let sourceDescription = source.map { $0.description } ?? ""
+        let destinationDescription = destination.map { $0.description } ?? ""
+        return "\t\"" + sourceDescription + "\" -> \"" + destinationDescription + "\";"
+    })
+    return "digraph tesseract {\n\(edges)\n}"
 }
 
 
