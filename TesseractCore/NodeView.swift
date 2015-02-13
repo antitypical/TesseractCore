dba1d07b13fb637c039377f5ad3c947c79dfac9f
Rob Rix <rob.rix@github.com>
2015-02-12 23:03:19 -0500

    NodeViews have inEdges.

--- TesseractCore/NodeView.swift
+++ TesseractCore/NodeView.swift
@@ -3,6 +3,10 @@
 public struct NodeView<T> {
 	public let graph: Graph<T>
 	public let identifier: Identifier
+
+	public var inEdges: SequenceOf<Edge> {
+		return SequenceOf(lazy(graph.edges).filter { $0.destination.identifier == self.identifier })
+	}
 }
 
 public func == <T: Equatable> (left: NodeView<T>, right: NodeView<T>) -> Bool {
