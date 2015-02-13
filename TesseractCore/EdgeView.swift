7ccc94a7ab6b81e8aa9af5b6f267998c4bf4e84e
Rob Rix <rob.rix@github.com>
2015-02-12 23:55:53 -0500

    EdgeView is Equatable.

--- TesseractCore/EdgeView.swift
+++ TesseractCore/EdgeView.swift
@@ -1,6 +1,6 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
-public struct EdgeView<T> {
+public struct EdgeView<T>: Equatable {
 	public let graph: Graph<T>
 	internal let edge: Edge
 
@@ -11,4 +11,9 @@ public struct EdgeView<T> {
 	public var destination: (NodeView<T>, Int) {
 		return (graph[edge.destination.identifier]!, edge.destination.inputIndex)
 	}
+}
+
+
+public func == <T> (left: EdgeView<T>, right: EdgeView<T>) -> Bool {
+	return left.edge == right.edge
 }
