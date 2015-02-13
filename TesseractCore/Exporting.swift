626b1a424f87c62babfcfb20a5b49ecca5e3e0b5
Rob Rix <rob.rix@github.com>
2015-02-13 00:57:17 -0500

    Serialize all nodes.

--- TesseractCore/Exporting.swift
+++ TesseractCore/Exporting.swift
@@ -1,19 +1,28 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
 public func exportDOT<T>(name: String, graph: Graph<T>) -> String {
-	let edges = join("\n", lazy(graph.edges).map(serializeEdge <| graph))
-	return "digraph \(name) {\n\(edges)\n}"
+	// NB: this would be lazy(graph).map(…) but for a compiler crash in Swift 1.2b1
+	let nodes = join("\n", map(graph) { join("\n", serialize($0).map { "\t\($0);" }) })
+	return "digraph \(name) {\n\(nodes)\n}"
 }
 
+private func serialize(identifier: Identifier) -> String {
+	return "\"\(identifier)\""
+}
 
-private func serializeEdge<T>(graph: Graph<T>, edge: Edge) -> String {
-	let node = serializeNode <| graph
-	return "\t\(node(edge.source.identifier)) -> \(node(edge.destination.identifier))\(serializeAttributes(attributes(edge)));"
+private func serialize<T>(node: NodeView<T>) -> [String] {
+	let attributes = ["label": "\(node.value)"]
+	let edges = join([], lazy(node.outEdges).map { join([], map($1, serialize)) })
+	return ["\(serialize(node.identifier))\(serialize(attributes))"] + edges
+}
+
+private func serialize<T>(edge: EdgeView<T>) -> [String] {
+	return ["\(serialize(edge.edge.source.identifier)) -> \(serialize(edge.edge.destination.identifier))\(serialize(attributes(edge.edge)))"]
 }
 
 private typealias Attributes = [String: String]
 
-private func serializeAttributes(attributes: Attributes) -> String {
+private func serialize(attributes: Attributes) -> String {
 	let serialized = join(",", lazy(attributes).map { "\($0)=\($1)" })
 	return attributes.count > 0 ? " [\(serialized)]" : ""
 }
@@ -24,12 +33,7 @@ private func attributes(edge: Edge) -> Attributes {
 		"sametail": toString(edge.source.outputIndex),
 		"headlabel": toString(edge.destination.inputIndex),
 		"samehead": toString(edge.destination.inputIndex),
-		"labeldistance": "2",
 	]
-}
-
-private func serializeNode<T>(graph: Graph<T>, identifier: Identifier) -> String {
-	return "\"\(graph.nodes[identifier]!)\""
 }
 
 
