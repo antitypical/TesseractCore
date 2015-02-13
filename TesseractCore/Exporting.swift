dafe0eb45e1d8280163d101ba1ce42235392db60
Rob Rix <rob.rix@github.com>
2015-02-12 21:18:52 -0500

    Condense exporting.

--- TesseractCore/Exporting.swift
+++ TesseractCore/Exporting.swift
@@ -1,13 +1,8 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
+
 public func exportDOT<T>(graph: Graph<T>) -> String {
 	let edges = join("\n", lazy(graph.edges).map { edge in
-		let sourceID = edge.source.identifier
-		let destinationID = edge.destination.identifier
-		let source = graph.nodes[sourceID]
-		let destination = graph.nodes[destinationID]
-		let sourceDescription = source.map(toString) ?? ""
-		let destinationDescription = destination.map(toString) ?? ""
-		return "\t\"" + sourceDescription + "\" -> \"" + destinationDescription + "\";"
+		"\t\"\(graph.nodes[edge.source.identifier]!)\" -> \"\(graph.nodes[edge.destination.identifier]!)\";"
 	})
 	return "digraph tesseract {\n\(edges)\n}"
 }
