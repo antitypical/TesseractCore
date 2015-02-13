61f5274d899adab2fc44c6b445096510bf60046f
Rob Rix <rob.rix@github.com>
2015-02-13 00:01:35 -0500

    EdgeView is Hashable.

--- TesseractCore/EdgeView.swift
+++ TesseractCore/EdgeView.swift
@@ -1,6 +1,6 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
-public struct EdgeView<T>: Equatable {
+public struct EdgeView<T>: Hashable {
 	public let graph: Graph<T>
 	internal let edge: Edge
 
@@ -11,8 +11,17 @@ public struct EdgeView<T>: Equatable {
 	public var destination: (NodeView<T>, Int) {
 		return (graph[edge.destination.identifier]!, edge.destination.inputIndex)
 	}
+
+
+	// MARK: HashValue
+
+	public var hashValue: Int {
+		return edge.hashValue
+	}
 }
 
+
+// MARK: Equatable
 
 public func == <T> (left: EdgeView<T>, right: EdgeView<T>) -> Bool {
 	return left.edge == right.edge
