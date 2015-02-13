ec61e9d77dc283848496586f4a3b1abf232f8127
Rob Rix <rob.rix@github.com>
2015-02-12 22:02:04 -0500

    Serialize attributes for edges.

--- TesseractCore/Exporting.swift
+++ TesseractCore/Exporting.swift
@@ -8,7 +8,24 @@ public func exportDOT<T>(graph: Graph<T>) -> String {
 
 private func serializeEdge<T>(graph: Graph<T>, edge: Edge) -> String {
 	let node = serializeNode <| graph
-	return "\t\(node(edge.source.identifier)) -> \(node(edge.destination.identifier));"
+	return "\t\(node(edge.source.identifier)) -> \(node(edge.destination.identifier))\(serializeAttributes(attributes(edge)));"
+}
+
+private typealias Attributes = [String: String]
+
+private func serializeAttributes(attributes: Attributes) -> String {
+	let serialized = join(",", lazy(attributes).map { "\($0)=\($1)" })
+	return attributes.count > 0 ? " [\(serialized)]" : ""
+}
+
+private func attributes(edge: Edge) -> Attributes {
+	return [
+		"taillabel": toString(edge.source.outputIndex),
+		"sametail": toString(edge.source.outputIndex),
+		"headlabel": toString(edge.destination.inputIndex),
+		"samehead": toString(edge.destination.inputIndex),
+		"labeldistance": "2",
+	]
 }
 
 private func serializeNode<T>(graph: Graph<T>, identifier: Identifier) -> String {
