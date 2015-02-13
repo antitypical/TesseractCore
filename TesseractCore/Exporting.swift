ffda139fc33d3c14425f45cc60407e81a5f86a5f
Rob Rix <rob.rix@github.com>
2015-02-12 21:22:06 -0500

    Extract edge serialization.

--- TesseractCore/Exporting.swift
+++ TesseractCore/Exporting.swift
@@ -1,10 +1,13 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
 public func exportDOT<T>(graph: Graph<T>) -> String {
-	let edges = join("\n", lazy(graph.edges).map { edge in
-		"\t\"\(graph.nodes[edge.source.identifier]!)\" -> \"\(graph.nodes[edge.destination.identifier]!)\";"
-	})
+	let edges = join("\n", lazy(graph.edges).map(serializeEdge <| graph))
 	return "digraph tesseract {\n\(edges)\n}"
+}
+
+
+private func serializeEdge<T>(graph: Graph<T>, edge: Edge) -> String {
+	return "\t\"\(graph.nodes[edge.source.identifier]!)\" -> \"\(graph.nodes[edge.destination.identifier]!)\";"
 }
 
 
