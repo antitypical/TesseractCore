bbfb984364a56f6fe2fb6aa1a04b4407cac6be53
Rob Rix <rob.rix@github.com>
2015-02-25 00:42:39 -0500

    Add parsers for attributes.

--- TesseractCore/Importing.swift
+++ TesseractCore/Importing.swift
@@ -10,6 +10,12 @@ private let string: Parser<String, String>.Function = quot ++ word ++ quot
 
 private let title: Parser<String, String>.Function = ignore("digraph ") ++ word ++ ignore(" {")
 private let edge: Parser<String, (String, String)>.Function = ws ++ string ++ ignore(" -> ") ++ string ++ ignore(";\n")
+
+private let attribute: Parser<String, (String, String)>.Function = word ++ ignore("=") ++ word
+private let comma: Parser<String, Ignore>.Function = ws ++ ignore(",") ++ ws
+private let attributeList: Parser<String, [(String, String)]>.Function = attribute ++ (comma ++ attribute)* --> { [ $0 ] + $1 }
+private let attributes: Parser<String, [String: String]>.Function = ws ++ ignore("[") ++ (attributeList --> { Dictionary($2) }) ++ ignore("]")
+
 private let graph: Parser<String, Graph<String>>.Function = edge+ --> { _, _, edges in
 	let nodeData = lazy(Set(lazy(edges).flatMap { [$0, $1] })).map { (Identifier(), $0) }
 	let nodeIdentifiers = Dictionary(nodeData.map(swap))
