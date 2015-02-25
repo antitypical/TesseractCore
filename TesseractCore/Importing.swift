95d569000f7144a142371400c14a458009f3270a
Rob Rix <rob.rix@github.com>
2015-02-25 00:20:13 -0500

    Rewrite importing.

--- TesseractCore/Importing.swift
+++ TesseractCore/Importing.swift
@@ -1,70 +1,33 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
-private func unique<T: Hashable>(list: [T]) -> [T] {
-	return Array(Set(list))
-}
+private let digit: Parser<String, String>.Function = %("0"..."9")
+private let lowercase: Parser<String, String>.Function = %("a"..."z")
+private let uppercase: Parser<String, String>.Function = %("A"..."Z")
+private let ws: Parser<String, Ignore>.Function = ignore(%" " | %"\t" | %"\n")+
+private let quot: Parser<String, Ignore>.Function = ignore("\"")
+private let word: Parser<String, String>.Function = (lowercase | uppercase | digit)+ --> { "".join($0) }
 
-public func parseTitle(line: String) -> String? {
-	let characterParser = %("0"..."9") | %("a"..."z") | %("A"..."Z")
-	let titleParser = ignore("digraph ") ++ characterParser+ ++ ignore(" {")
-	if let rawTitle = titleParser(line)?.0 {
-		return rawTitle.reduce("", combine: +)
-	} else {
-		return nil
+private let title: Parser<String, String>.Function = ignore("digraph ") ++ word ++ ignore(" {")
+private let edge: Parser<String, (String, String)>.Function = ws ++ quot ++ word ++ ignore("\" -> \"") ++ word ++ ignore("\";")
+private let graph: Parser<String, Graph<String>>.Function = edge+ --> { _, _, edges in
+	let nodeData = lazy(Set(lazy(edges).flatMap { [$0, $1] })).map { (Identifier(), $0) }
+	let nodeIdentifiers = Dictionary(nodeData.map(swap))
+	let nodes = Dictionary(nodeData)
+	let ed = lazy(edges).map { source, destination in
+		(nodeIdentifiers[source] &&& nodeIdentifiers[destination]).map { Edge(($0, 0), ($1, 0)) }
 	}
-}
-
-public func parseEdge(edge: String) -> (String, String)? {
-	let characterParser = %("0"..."9") | %("a"..."z") | %("A"..."Z")
-	let ignoreBeginningOfLine = ignore("\t") ++ ignore("\"")
-	let edgeParser = ignoreBeginningOfLine ++ characterParser+ ++ ignore("\" -> \"") ++ characterParser+ ++ ignore("\";");
-
-	if let (rawSource, rawDestination) = edgeParser(edge)?.0 {
-		let source = String(rawSource.reduce([], combine: +))
-		let destination = String(rawDestination.reduce([], combine: +))
-		return (source, destination)
+	let edges: Set<Edge> = reduce(ed, []) { edges, edge in
+		edges.union(edge.map { [ $0 ] } ?? [])
 	}
-
-	return nil
+	return Graph(nodes: nodes, edges: edges)
 }
 
 // GraphViz (.dot) file spec: http://graphviz.org/content/dot-language
-public func importDOT(file: String) -> (String, Graph<String>) {
-	let lines = split(file, { $0 == "\n" })
-	let name = parseTitle(lines[0]) ?? ""
-	// Skip the title.
-	let rawLines = lines[1...(lines.count - 2)]
-	let rawEdges = map(rawLines) { parseEdge($0) ?? ("", "")}
-	let rawSources = map(rawEdges) { $0.0 }
-	let rawDestinations = map(rawEdges) { edge in edge.1 }
-	let rawNodes = rawSources + rawDestinations
-	let uniqueNodes = unique(rawNodes)
-	let nodeIdentifiers = map(uniqueNodes) { _ in Identifier() }
-	var inputCount = map(uniqueNodes, const(0))
-	var outputCount = inputCount
-    
-	let edges: [Edge] = map(rawEdges) { edge in
-		let (sourceString, destinationString) = edge
-		let sourceIndex = find(uniqueNodes, sourceString) ?? 0
-		let destinationIndex = find(uniqueNodes, destinationString) ?? 0
-		let sourceOutputNumber = outputCount[sourceIndex] + 1
-		let destinationInputNumber = inputCount[sourceIndex] + 1
-		let source = (nodeIdentifiers[sourceIndex], sourceOutputNumber)
-		let destination = (nodeIdentifiers[destinationIndex], destinationInputNumber)
-		return Edge(source, destination)
-	}
-	
-	let nodes = uniqueNodes.reduce(Dictionary<Identifier, String>()) { (accum: [Identifier: String], curr: String) in
-		let identifierIndex = find(uniqueNodes, curr) ?? 0
-		let identifier = nodeIdentifiers[identifierIndex]
-		return accum + [identifier: curr]
-    }
-    
-	return (name, Graph(nodes: nodes, edges: Set(edges)))
+public func importDOT(string: String) -> (String, Graph<String>)? {
+	return parse(title ++ graph ++ ignore("}"), string)
 }
 
 // MARK: - Imports
 
 import Prelude
-import Set
 import Madness
