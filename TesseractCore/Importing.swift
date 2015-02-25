2dab3f99ac984381ce974eb44642393c54911aee
Rob Rix <rob.rix@github.com>
2015-02-25 01:01:55 -0500

    Parse attributes.

--- TesseractCore/Importing.swift
+++ TesseractCore/Importing.swift
@@ -9,18 +9,19 @@ private let word: Parser<String, String>.Function = (lowercase | uppercase | dig
 private let string: Parser<String, String>.Function = quot ++ word ++ quot
 
 private let title: Parser<String, String>.Function = ignore("digraph ") ++ word ++ ignore(" {")
-private let edge: Parser<String, (String, String)>.Function = ws ++ string ++ ignore(" -> ") ++ string ++ ignore(";\n")
+private let sourceAndIdentifier: Parser<String, (String, String)>.Function = string ++ ignore(" -> ") ++ string
+private let edge: Parser<String, (String, String, [String: String])>.Function = ws ++ sourceAndIdentifier ++ ws ++ attributes ++ ignore(";\n") --> { ($0.0, $0.1, $1) }
 
 private let attribute: Parser<String, (String, String)>.Function = word ++ ignore("=") ++ word
 private let comma: Parser<String, Ignore>.Function = ws ++ ignore(",") ++ ws
-private let attributeList: Parser<String, [(String, String)]>.Function = attribute ++ (comma ++ attribute)* --> { [ $0 ] + $1 }
-private let attributes: Parser<String, [String: String]>.Function = ws ++ ignore("[") ++ (attributeList --> { Dictionary($2) }) ++ ignore("]")
+private let attributeList: Parser<String, [String: String]>.Function = attribute ++ (comma ++ attribute)* --> { Dictionary([ $0 ] + $1) }
+private let attributes: Parser<String, [String: String]>.Function = (ignore("[") ++ attributeList ++ ignore("]")) | { _, index in ([:], index) }
 
 private let graph: Parser<String, Graph<String>>.Function = edge+ --> { _, _, edges in
-	let nodeData = lazy(Set(lazy(edges).flatMap { [$0, $1] })).map { (Identifier(), $0) }
+	let nodeData = lazy(Set(lazy(edges).flatMap { source, destination, _ in [source, destination] })).map { (Identifier(), $0) }
 	let nodeIdentifiers = Dictionary(nodeData.map(swap))
 	let nodes = Dictionary(nodeData)
-	let ed = lazy(edges).map { source, destination in
+	let ed = lazy(edges).map { source, destination, attributes in
 		(nodeIdentifiers[source] &&& nodeIdentifiers[destination]).map { Edge(($0, 0), ($1, 0)) }
 	}
 	let edges: Set<Edge> = reduce(ed, []) { edges, edge in
