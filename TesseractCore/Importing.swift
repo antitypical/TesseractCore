97c6b53f6145eb7d661308d6b4b8f3e30915c65c
Rob Rix <rob.rix@github.com>
2015-02-25 22:21:26 -0500

    Apply the attributes in the edge parser.

--- TesseractCore/Importing.swift
+++ TesseractCore/Importing.swift
@@ -27,25 +27,22 @@ private let string: Parser<String, String>.Function = quot ++ word ++ quot
 private let title: Parser<String, String>.Function = ignore("digraph ") ++ word ++ ignore(" {")
 private let sourceAndIdentifier: Parser<String, (String, String)>.Function = string ++ ignore(" -> ") ++ string
 private let terminator = ignore(";") ++ ws
-private let edge: Parser<String, (String, String, [String: String])>.Function = ws ++ sourceAndIdentifier ++ ws ++ attributes ++ terminator --> { ($0.0, $0.1, $1) }
+private let edge: Parser<String, ((String, Int), (String, Int))>.Function = ws ++ sourceAndIdentifier ++ ws ++ attributes ++ terminator --> { (($0.0, $1["sametail"]?.toInt() ?? 0), ($0.1, $1["headlabel"]?.toInt() ?? 0)) }
 
 private let attribute: Parser<String, (String, String)>.Function = word ++ ignore("=") ++ word
 private let comma: Parser<String, Ignore>.Function = ws ++ ignore(",") ++ ws
 private let attributeList: Parser<String, [String: String]>.Function = attribute ++ (comma ++ attribute)* --> { Dictionary([ $0 ] + $1) }
 private let attributes: Parser<String, [String: String]>.Function = (ignore("[") ++ attributeList ++ ignore("]")) | { _, index in ([:], index) }
 
-private let graph: Parser<String, Graph<String>>.Function = edge+ --> { _, _, edges in
-	let nodeData = lazy(Set(lazy(edges).flatMap { source, destination, _ in [source, destination] })).map { (Identifier(), $0) }
+private let graph: Parser<String, Graph<String>>.Function = edge+ --> { _, _, edgeParses in
+	let nodeData = lazy(Set(lazy(edgeParses).flatMap { source, destination in [source.0, destination.0] })).map { (Identifier(), $0) }
 	let nodeIdentifiers = Dictionary(nodeData.map(swap))
 	let nodes = Dictionary(nodeData)
-	let ed = lazy(edges).map { source, destination, attributes in
-		(nodeIdentifiers[source] &&& nodeIdentifiers[destination]).map {
-			Edge(($0, attributes["sametail"]?.toInt() ?? 0), ($1, attributes["headlabel"]?.toInt() ?? 0))
-		}
-	}
-	let edges: Set<Edge> = reduce(ed, []) { edges, edge in
-		edges.union(edge.map { [ $0 ] } ?? [])
-	}
+	let edges: Set<Edge> = reduce(lazy(edgeParses).map { source, destination in
+		(nodeIdentifiers[source.0] &&& nodeIdentifiers[destination.0]).map {
+			[ Edge(($0, source.1), ($1, destination.1)) ]
+		} ?? []
+	}, [], { $0.union($1) })
 	return Graph(nodes: nodes, edges: edges)
 }
 
