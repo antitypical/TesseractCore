cfd74cfbe46eb8d18c00869581dda57819b7be9d
Rob Rix <rob.rix@github.com>
2015-03-08 19:41:51 -0400

    Add differentials for Set.

--- TesseractCore/Diffs.swift
+++ TesseractCore/Diffs.swift
@@ -5,3 +5,20 @@ public protocol DifferentialType {
 
 	static func differentiate(#before: Differentiable, after: Differentiable) -> Self
 }
+
+public struct SetDifferential<T: Hashable>: DifferentialType {
+	public init(inserted: Set<T>, deleted: Set<T>) {
+		self.inserted = inserted
+		self.deleted = deleted
+	}
+
+	public let inserted: Set<T>
+	public let deleted: Set<T>
+
+
+	// MARK: DifferentialType
+
+	public static func differentiate(#before: Set<T>, after: Set<T>) -> SetDifferential {
+		return SetDifferential(inserted: after.subtract(before), deleted: before.subtract(after))
+	}
+}
