828e29f012f1119d80459ed51d603565ffa951dd
Rob Rix <rob.rix@github.com>
2015-03-08 19:42:12 -0400

    Add differentials for Dictionary.

--- TesseractCore/Diffs.swift
+++ TesseractCore/Diffs.swift
@@ -23,6 +23,25 @@ public struct SetDifferential<T: Hashable>: DifferentialType {
 	}
 }
 
+public struct DictionaryDifferential<Key: Hashable, Value>: DifferentialType {
+	public init(inserted: [Key: Value], deleted: Set<Key>) {
+		self.inserted = inserted
+		self.deleted = deleted
+	}
+
+	public let inserted: [Key: Value]
+	public let deleted: Set<Key>
+
+
+	// MARK: DifferentialType
+
+	public static func differentiate(#before: Dictionary<Key, Value>, after: Dictionary<Key, Value>) -> DictionaryDifferential {
+		let (beforeKeys, afterKeys) = (Set(before.keys), Set(after.keys))
+
+		return DictionaryDifferential(inserted: after[afterKeys.subtract(beforeKeys)], deleted: beforeKeys.subtract(afterKeys))
+	}
+}
+
 extension Dictionary {
 	subscript (keys: Set<Key>) -> Dictionary {
 		var result: Dictionary = [:]
