f6a0127d55a704cb93ad62be1ec4829168baeb54
Rob Rix <rob.rix@github.com>
2015-03-07 21:35:59 -0500

    Function types don’t have parameter names.

--- TesseractCore/Type.swift
+++ TesseractCore/Type.swift
@@ -1,12 +1,8 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
 public enum Type: Hashable, IntegerLiteralConvertible, Printable {
-	public init(function name: String, _ from: Type, _ to: Type) {
-		self = Function(name, Box(from), Box(to))
-	}
-
 	public init(function from: Type, _ to: Type) {
-		self = Function(nil, Box(from), Box(to))
+		self = Function(Box(from), Box(to))
 	}
 
 	public init(integerLiteral value: IntegerLiteralType) {
@@ -17,7 +13,7 @@ public enum Type: Hashable, IntegerLiteralConvertible, Printable {
 	case Unit
 	case Boolean
 	case Parameter(Int)
-	case Function(String?, Box<Type>, Box<Type>)
+	case Function(Box<Type>, Box<Type>)
 
 
 	public var arity: Int {
@@ -26,7 +22,7 @@ public enum Type: Hashable, IntegerLiteralConvertible, Printable {
 
 	public var functionType: (Type, Type)? {
 		switch self {
-		case let Function(_, t1, t2):
+		case let Function(t1, t2):
 			return (t1.value, t2.value)
 		default:
 			return nil
@@ -34,17 +30,13 @@ public enum Type: Hashable, IntegerLiteralConvertible, Printable {
 	}
 
 
-	private func parameters(n: Int) -> [(String, Type)] {
+	public var parameters: [Type] {
 		switch self {
-		case let Function(name, t1, t2):
-			return [(name ?? n.description, t1.value)] + t2.value.parameters(name == nil ? 1 : 0)
+		case let Function(t1, t2):
+			return [t1.value] + t2.value.parameters
 		default:
 			return []
 		}
-	}
-
-	public var parameters: [(String, Type)] {
-		return parameters(0)
 	}
 
 
@@ -54,7 +46,7 @@ public enum Type: Hashable, IntegerLiteralConvertible, Printable {
 		switch self {
 		case let Parameter(index):
 			return 380371373 ^ index
-		case let Function(_, x, y):
+		case let Function(x, y):
 			return 8471823991 ^ x.value.hashValue ^ y.value.hashValue
 		case Boolean:
 			return 6504993773
@@ -70,8 +62,8 @@ public enum Type: Hashable, IntegerLiteralConvertible, Printable {
 		switch self {
 		case let Parameter(index):
 			return index.description
-		case let Function(name, parameterType, returnType):
-			return "\(name): \(parameterType) → \(returnType)"
+		case let Function(parameterType, returnType):
+			return "\(parameterType) → \(returnType)"
 		case Boolean:
 			return "Boolean"
 		case Unit:
@@ -85,7 +77,7 @@ public func == (left: Type, right: Type) -> Bool {
 	switch (left, right) {
 	case let (.Parameter(x), .Parameter(y)):
 		return x == y
-	case let (.Function(_, x1, x2), .Function(_, y1, y2)):
+	case let (.Function(x1, x2), .Function(y1, y2)):
 		return x1 == y1 && x2 == y2
 	case (.Unit, .Unit), (.Boolean, .Boolean):
 		return true
