4fd2f0a371e8facd4f50954b10c158ddeb6c1243
Rob Rix <rob.rix@github.com>
2015-03-10 19:17:05 -0400

    FloatingPointDifferentials.

---
+++ TesseractCore/FloatingPointDifferential.swift
@@ -0,0 +1,12 @@
+//  Copyright (c) 2015 Rob Rix. All rights reserved.
+
+public struct FloatingPointDifferential<FP: FloatingPointType where FP.Stride == FP>: DifferentialType {
+	public let delta: FP
+
+
+	// MARK: DifferentialType
+
+	public static func differentiate(#before: FP, after: FP) -> FloatingPointDifferential {
+		return FloatingPointDifferential(delta: after - before)
+	}
+}
