0333e555c210cf873163245132ca3e2c2e7d73f8
Rob Rix <rob.rix@github.com>
2015-03-09 22:42:48 -0400

    Move GraphDifferential to its own file.

--- TesseractCore/Diffs.swift
+++ TesseractCore/Diffs.swift
@@ -5,17 +5,3 @@ public protocol DifferentialType {
 
 	static func differentiate(#before: Differentiable, after: Differentiable) -> Self
 }
-
-
-// MARK: - Graph
-
-public struct GraphDifferential<T>: DifferentialType {
-	public let nodes: DictionaryDifferential<Identifier, T>
-	public let edges: SetDifferential<Edge>
-
-	// MARK: DifferentialType
-
-	public static func differentiate(#before: Graph<T>, after: Graph<T>) -> GraphDifferential {
-		return GraphDifferential(nodes: .differentiate(before: before.nodes, after: after.nodes), edges: .differentiate(before: before.edges, after: after.edges))
-	}
-}
