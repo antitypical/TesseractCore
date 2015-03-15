5634117cfb8cc1e97097641f5701a5529f8558f9
Rob Rix <rob.rix@github.com>
2015-03-15 15:07:03 -0400

    Mark find’s predicate as noescape.

--- TesseractCore/Find.swift
+++ TesseractCore/Find.swift
@@ -1,6 +1,6 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
-public func find<C: CollectionType>(domain: C, predicate: C.Generator.Element -> Bool) -> C.Index? {
+public func find<C: CollectionType>(domain: C, @noescape predicate: C.Generator.Element -> Bool) -> C.Index? {
 	for index in domain.startIndex..<domain.endIndex {
 		if predicate(domain[index]) { return index }
 	}
