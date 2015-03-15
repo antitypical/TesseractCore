1aada950dac9fd4d1b4bcfbc39f053ce43fcaf6d
Rob Rix <rob.rix@github.com>
2015-03-15 17:49:26 -0400

    DictionaryDifferential produces UnorderedDifferentials.

--- TesseractCore/DictionaryDifferential.swift
+++ TesseractCore/DictionaryDifferential.swift
@@ -1,20 +1,11 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
 public struct DictionaryDifferential<Key: Hashable, Value: Equatable> {
-	public init(inserted: [Key: Value], deleted: [Key: Value]) {
-		self.inserted = inserted
-		self.deleted = deleted
-	}
-
-	public let inserted: [Key: Value]
-	public let deleted: [Key: Value]
-
-
-	public static func differentiate(#before: Dictionary<Key, Value>, after: Dictionary<Key, Value>) -> DictionaryDifferential {
+	public static func differentiate(#before: Dictionary<Key, Value>, after: Dictionary<Key, Value>) -> UnorderedDifferential<(Key, Value)> {
 		let (beforeKeys, afterKeys) = (Set(before.keys), Set(after.keys))
 		let changedKeys = Set(lazy(beforeKeys.intersect(afterKeys))
 			.filter { before[$0] != after[$0] })
 
-		return DictionaryDifferential(inserted: after[afterKeys.subtract(beforeKeys).union(changedKeys)], deleted: before[beforeKeys.subtract(afterKeys).union(changedKeys)])
+		return UnorderedDifferential(inserted: after[afterKeys.subtract(beforeKeys).union(changedKeys)], deleted: before[beforeKeys.subtract(afterKeys).union(changedKeys)])
 	}
 }
--- TesseractCore/GraphDifferential.swift
+++ TesseractCore/GraphDifferential.swift
@@ -1,11 +1,11 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
 public struct GraphDifferential<T: Equatable> {
-	public let nodes: DictionaryDifferential<Identifier, T>
+	public let nodes: UnorderedDifferential<(Identifier, T)>
 	public let edges: UnorderedDifferential<Edge>
 
 
 	public static func differentiate(#before: Graph<T>, after: Graph<T>) -> GraphDifferential {
-		return GraphDifferential(nodes: .differentiate(before: before.nodes, after: after.nodes), edges: SetDifferential.differentiate(before: before.edges, after: after.edges))
+		return GraphDifferential(nodes: DictionaryDifferential.differentiate(before: before.nodes, after: after.nodes), edges: SetDifferential.differentiate(before: before.edges, after: after.edges))
 	}
 }
