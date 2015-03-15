2f3e964b0c6ecfda8e8cfdb0467586b5a15fe8f1
Rob Rix <rob.rix@github.com>
2015-03-15 17:44:22 -0400

    Floating point diffs just return the delta directly.

--- TesseractCore/FloatingPointDifferential.swift
+++ TesseractCore/FloatingPointDifferential.swift
@@ -1,12 +1,9 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
 public struct FloatingPointDifferential<FP: FloatingPointType where FP.Stride == FP>: DifferentiatorType {
-	public let delta: FP
-
-
 	// MARK: DifferentialType
 
-	public static func differentiate(#before: FP, after: FP) -> FloatingPointDifferential {
-		return FloatingPointDifferential(delta: after - before)
+	public static func differentiate(#before: FP, after: FP) -> FP {
+		return after - before
 	}
 }
