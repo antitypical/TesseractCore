b46a232f0bd4fb21dd06fae1e80c37ea1091490d
Rob Rix <rob.rix@github.com>
2015-03-18 23:28:10 -0400

    Graph differentiators do not moonlight as differentials.

--- TesseractCore/GraphDifferentiator.swift
+++ TesseractCore/GraphDifferentiator.swift
@@ -1,11 +1,10 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
+
 public struct GraphDifferentiator<T: Equatable> {
-	public let nodes: UnorderedDifferential<(Identifier, T)>
-	public let edges: UnorderedDifferential<Edge>
+	public typealias Differential = (nodes: UnorderedDifferential<(Identifier, T)>, edges: UnorderedDifferential<Edge>)
 
-
-	public static func differentiate(#before: Graph<T>, after: Graph<T>) -> GraphDifferentiator {
-		return GraphDifferentiator(nodes: DictionaryDifferentiator.differentiate(before: before.nodes, after: after.nodes), edges: SetDifferentiator.differentiate(before: before.edges, after: after.edges))
+	public static func differentiate(#before: Graph<T>, after: Graph<T>) -> Differential {
+		return Differential(nodes: DictionaryDifferentiator.differentiate(before: before.nodes, after: after.nodes), edges: SetDifferentiator.differentiate(before: before.edges, after: after.edges))
 	}
 }
