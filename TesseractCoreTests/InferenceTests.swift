8aa3aab51e967990e6b5f1f64d877764c6de6055
Rob Rix <rob.rix@github.com>
2015-03-24 01:31:43 -0400

    Use constraint generation directly.

--- TesseractCoreTests/InferenceTests.swift
+++ TesseractCoreTests/InferenceTests.swift
@@ -2,43 +2,32 @@
 
 final class InferenceTests: XCTestCase {
 	func testGraphsWithoutReturnsHaveUnitType() {
-		assert(type(Graph()), ==, .Unit)
+		assert(constraints(Graph()).0, ==, .Unit)
 	}
 
 	func testGraphsWithOneReturnHaveVariableType() {
-		assert(type(Graph(nodes: [Identifier(): .Return((0, .Unit))])), ==, 0)
+		assert(constraints(Graph(nodes: [Identifier(): .Return((0, .Unit))])).0, ==, 0)
 	}
 
 	func testGraphsWithOneParameterAndNoReturnsHaveFunctionTypeReturningUnit() {
-		assert(type(Graph(nodes: [Identifier(): .Parameter((0, .Unit))])), ==, .function(0, .Unit))
+		assert(constraints(Graph(nodes: [Identifier(): .Parameter((0, .Unit))])).0, ==, .function(0, .Unit))
 	}
 
 	func testGraphsWithOneParameterAndOneReturnHaveFunctionTypeFromVariableToDifferentVariable() {
-		assert(type(Graph(nodes: [Identifier(): .Parameter((0, .Unit)), Identifier(): .Return((0, .Unit))])), ==, .function(1, 0))
+		assert(constraints(Graph(nodes: [Identifier(): .Parameter((0, .Unit)), Identifier(): .Return((0, .Unit))])).0, ==, .function(1, 0))
 	}
 
 	func testGraphsWithMultipleParametersHaveCurriedFunctionType() {
-		assert(type(Graph(nodes: [Identifier(): .Parameter((0, .Unit)), Identifier(): .Parameter((1, .Unit))])), ==, .function(1, .function(0, .Unit)))
+		assert(constraints(Graph(nodes: [Identifier(): .Parameter((0, .Unit)), Identifier(): .Parameter((1, .Unit))])).0, ==, .function(1, .function(0, .Unit)))
 	}
 
 	func testGraphsWithMultipleReturnsHaveProductType() {
-		assert(type(Graph(nodes: [Identifier(): .Return((0, .Unit)), Identifier(): .Return((1, .Unit))])), ==, .product(1, 0))
+		assert(constraints(Graph(nodes: [Identifier(): .Return((0, .Unit)), Identifier(): .Return((1, .Unit))])).0, ==, .product(1, 0))
 	}
 
 	func testGraphsWithMultipleParametersAndMultipleReturnsHaveCurriedFunctionTypeProducingProductType() {
-		assert(type(Graph(nodes: [Identifier(): .Parameter((0, .Unit)), Identifier(): .Parameter((1, .Unit)), Identifier(): .Return((0, .Unit)), Identifier(): .Return((1, .Unit))])), ==, .function(3, .function(2, .product(1, 0))))
+		assert(constraints(Graph(nodes: [Identifier(): .Parameter((0, .Unit)), Identifier(): .Parameter((1, .Unit)), Identifier(): .Return((0, .Unit)), Identifier(): .Return((1, .Unit))])).0, ==, .function(3, .function(2, .product(1, 0))))
 	}
-}
-
-
-// MARK: Helpers
-
-private func type(graph: Graph<Node>) -> Term {
-	return simplify(constraints(graph).0)
-}
-
-private func simplify(type: Term) -> Term {
-	return Substitution(lazy(enumerate(type.freeVariables |> (flip(sorted) <| { $0.value < $1.value }))).map { ($1, Term(integerLiteral: $0)) }).apply(type)
 }
 
 
