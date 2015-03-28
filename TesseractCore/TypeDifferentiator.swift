40a597890d9ef50fff2691de01a20f3c1a7df602
Rob Rix <rob.rix@github.com>
2015-03-28 01:19:01 -0400

    Add a type differential.

---
+++ TesseractCore/TypeDifferentiator.swift
@@ -0,0 +1,9 @@
+//  Copyright (c) 2015 Rob Rix. All rights reserved.
+
+public enum TypeDifferential {
+
+}
+
+
+import Manifold
+import Prelude
---
+++ TesseractCoreTests/TypeDifferentialTests.swift
@@ -0,0 +1,11 @@
+//  Copyright (c) 2015 Rob Rix. All rights reserved.
+
+final class TypeDifferentialTests: XCTestCase {
+
+}
+
+
+import Assertions
+import Manifold
+import TesseractCore
+import XCTest
