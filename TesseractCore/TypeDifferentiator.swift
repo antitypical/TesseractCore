ad58272891e472de1555b8168eddbd1fd7364519
Rob Rix <rob.rix@github.com>
2015-03-28 01:20:01 -0400

    TypeDifferential conforms to FixpointType.

--- TesseractCore/TypeDifferentiator.swift
+++ TesseractCore/TypeDifferentiator.swift
@@ -1,7 +1,24 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
-public enum TypeDifferential {
+public enum TypeDifferential: FixpointType {
 
+	// MARK: Cases
+
+	case Patch(Type<TypeDifferential>)
+	case Empty
+
+
+	// MARK: FixpointType
+
+	public typealias Recur = Type<TypeDifferential>
+
+	public static func In(type: Recur) -> TypeDifferential {
+		return .Patch(type)
+	}
+
+	public static func out(diff: TypeDifferential) -> Recur {
+		return diff.analysis(ifPatch: unit, ifEmpty: const(nil))!
+	}
 }
 
 
