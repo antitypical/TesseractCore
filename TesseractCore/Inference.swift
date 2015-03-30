b7062f19a13633fd069693b4784ef7606027c2db
Rob Rix <rob.rix@github.com>
2015-03-29 23:54:51 -0400

    Instantiate once and only once.

--- TesseractCore/Inference.swift
+++ TesseractCore/Inference.swift
@@ -9,27 +9,19 @@ public func typeOf(graph: Graph<Node>) -> Either<Error<Identifier>, Term> {
 
 
 public func constraints(graph: Graph<Node>) -> (Term, constraints: ConstraintSet) {
-	let parameters = Node.parameters(graph).map { typeOf(graph, $0.0)! }.reverse()
-	let returns = Node.returns(graph).map { typeOf(graph, $0.0)! }.reverse()
+	let instantiated = graph.map { $0.type.instantiate() }
 
-	let constraints = ConstraintSet(lazy(graph.edges).flatMap { [ typeOf(graph, $0.source.identifier)!.`return` === typeOf(graph, $0.destination)! ] })
+	let parameters = Node.parameters(graph).map { instantiated.nodes[$0.0]! }.reverse()
+	let returns = Node.returns(graph).map { instantiated.nodes[$0.0]! }.reverse()
+
+	let constraints = ConstraintSet(lazy(graph.edges).flatMap { (edge: Edge) -> [Constraint] in
+		let source = instantiated.nodes[edge.source.identifier]!.returns[edge.source.outputIndex]
+		let destination = instantiated.nodes[edge.destination.identifier].map { $0.parameters.isEmpty ? $0 : $0.parameters[edge.destination.inputIndex] }!
+		return [ source === destination ]
+	})
 
 	let returnType: Term = first(returns).map { reduce(dropFirst(returns), $0, flip(Term.product)) } ?? .Unit
 	return (reduce(parameters, returnType, flip(Term.function)), constraints)
-}
-
-
-private func typeOf(graph: Graph<Node>, identifier: Identifier) -> Term? {
-	return graph.nodes[identifier]?.type
-}
-
-/// Returns the type of the node input at `endpoint` in `graph`. For `return` nodes (which have no parameters), this will be the node’s type.
-private func typeOf(graph: Graph<Node>, endpoint: (identifier: Identifier, inputIndex: Int)) -> Term? {
-	return graph.nodes[endpoint.identifier].map {
-		$0.`return` != nil ?
-			$0.type.instantiate()
-		:	$0.type.parameters[endpoint.inputIndex].instantiate()
-	}
 }
 
 
