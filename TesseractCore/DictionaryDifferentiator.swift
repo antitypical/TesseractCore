7f87d86b66dd568611a379ba4d9038f72e0c3c60
Rob Rix <rob.rix@github.com>
2015-04-15 22:28:40 -0400

    Dictionary differentials do not require Equatable.

--- TesseractCore/DictionaryDifferentiator.swift
+++ TesseractCore/DictionaryDifferentiator.swift
@@ -1,6 +1,6 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
-public struct DictionaryDifferentiator<Key: Hashable, Value: Equatable> {
+public struct DictionaryDifferentiator<Key: Hashable, Value> {
 	public static func differentiate(#before: Dictionary<Key, Value>, after: Dictionary<Key, Value>, equals: (Value, Value) -> Bool) -> UnorderedDifferential<(Key, Value)> {
 		let (beforeKeys, afterKeys) = (Set(before.keys), Set(after.keys))
 		let changedKeys = Set(lazy(beforeKeys.intersect(afterKeys))
