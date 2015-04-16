7a98a88e42241f4be123babcbd21685b3c1fdaef
Rob Rix <rob.rix@github.com>
2015-04-15 22:36:31 -0400

    Graph differentials are parameterized by equality.

--- TesseractCore/GraphDifferentiator.swift
+++ TesseractCore/GraphDifferentiator.swift
@@ -3,8 +3,8 @@
 public struct GraphDifferentiator<C: CollectionType where C.Generator.Element: Equatable, C.Index: Hashable> {
 	public typealias Differential = (nodes: UnorderedDifferential<(C.Index, C.Generator.Element)>, edges: UnorderedDifferential<Edge<C>>)
 
-	public static func differentiate(#before: Graph<C>, after: Graph<C>) -> Differential {
-		let nodes = DictionaryDifferentiator.differentiate(before: Dictionary(zip(indices(before.nodes), before.nodes)), after: Dictionary(zip(indices(after.nodes), after.nodes)))
+	public static func differentiate(#before: Graph<C>, after: Graph<C>, equals: (C.Generator.Element, C.Generator.Element) -> Bool) -> Differential {
+		let nodes = DictionaryDifferentiator.differentiate(before: Dictionary(zip(indices(before.nodes), before.nodes)), after: Dictionary(zip(indices(after.nodes), after.nodes)), equals: equals)
 		return Differential(nodes: nodes, edges: SetDifferentiator.differentiate(before: before.edges, after: after.edges))
 	}
 }
