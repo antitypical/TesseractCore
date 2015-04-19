9c9924f243fd7748382da460dffa8ab5b65ac702
Rob Rix <rob.rix@github.com>
2015-04-19 14:03:10 -0400

    Graph diffs have an inverse.

--- TesseractCore/GraphDifferential.swift
+++ TesseractCore/GraphDifferential.swift
@@ -4,6 +4,14 @@ public struct GraphDifferential<C: CollectionType where C.Index: Hashable> {
 	public let nodes: UnorderedDifferential<(C.Index, C.Generator.Element)>
 	public let edges: UnorderedDifferential<Edge<C>>
 
+
+	public var inverse: GraphDifferential {
+		return GraphDifferential(nodes: nodes.inverse, edges: edges.inverse)
+	}
+
+
+	// MARK: Differentiation
+
 	public static func differentiate(#before: Graph<C>, after: Graph<C>, equals: (C.Generator.Element, C.Generator.Element) -> Bool) -> GraphDifferential {
 		let nodes = DictionaryDifferentiator.differentiate(before: Dictionary(zip(indices(before.nodes), before.nodes)), after: Dictionary(zip(indices(after.nodes), after.nodes)), equals: equals)
 		return GraphDifferential(nodes: nodes, edges: SetDifferentiator.differentiate(before: before.edges, after: after.edges))
