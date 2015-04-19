48df932a6f4d1658735d3e48c05be5b7ad680e3c
Rob Rix <rob.rix@github.com>
2015-04-19 13:58:49 -0400

    Really helps if this is public.

--- TesseractCore/GraphDifferential.swift
+++ TesseractCore/GraphDifferential.swift
@@ -1,8 +1,8 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
 public struct GraphDifferential<C: CollectionType where C.Index: Hashable> {
-	let nodes: UnorderedDifferential<(C.Index, C.Generator.Element)>
-	let edges: UnorderedDifferential<Edge<C>>
+	public let nodes: UnorderedDifferential<(C.Index, C.Generator.Element)>
+	public let edges: UnorderedDifferential<Edge<C>>
 
 	public static func differentiate(#before: Graph<C>, after: Graph<C>, equals: (C.Generator.Element, C.Generator.Element) -> Bool) -> GraphDifferential {
 		let nodes = DictionaryDifferentiator.differentiate(before: Dictionary(zip(indices(before.nodes), before.nodes)), after: Dictionary(zip(indices(after.nodes), after.nodes)), equals: equals)
