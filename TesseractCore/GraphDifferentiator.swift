c5525c217ea609cfa1c69b851345b78f7d9c4f5d
Rob Rix <rob.rix@github.com>
2015-04-19 13:56:38 -0400

    GraphDifferentiator → GraphDifferential.

--- TesseractCore/GraphDifferentiator.swift
+++ TesseractCore/GraphDifferentiator.swift
@@ -1,10 +1,11 @@
 //  Copyright (c) 2015 Rob Rix. All rights reserved.
 
-public struct GraphDifferentiator<C: CollectionType where C.Index: Hashable> {
-	public typealias Differential = (nodes: UnorderedDifferential<(C.Index, C.Generator.Element)>, edges: UnorderedDifferential<Edge<C>>)
+public struct GraphDifferential<C: CollectionType where C.Index: Hashable> {
+	let nodes: UnorderedDifferential<(C.Index, C.Generator.Element)>
+	let edges: UnorderedDifferential<Edge<C>>
 
-	public static func differentiate(#before: Graph<C>, after: Graph<C>, equals: (C.Generator.Element, C.Generator.Element) -> Bool) -> Differential {
+	public static func differentiate(#before: Graph<C>, after: Graph<C>, equals: (C.Generator.Element, C.Generator.Element) -> Bool) -> GraphDifferential {
 		let nodes = DictionaryDifferentiator.differentiate(before: Dictionary(zip(indices(before.nodes), before.nodes)), after: Dictionary(zip(indices(after.nodes), after.nodes)), equals: equals)
-		return Differential(nodes: nodes, edges: SetDifferentiator.differentiate(before: before.edges, after: after.edges))
+		return GraphDifferential(nodes: nodes, edges: SetDifferentiator.differentiate(before: before.edges, after: after.edges))
 	}
 }
